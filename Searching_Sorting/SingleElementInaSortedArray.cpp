6481732f8ce20ad65579ced2d42365f54a68fb49
AADITYA26 <dhannawataaditya@gmail.com>
2025-12-29 23:05:54 +0530

    handled edge cases for peak Index cod

--- Searching_Sorting/SingleElementInaSortedArray.cpp
+++ Searching_Sorting/SingleElementInaSortedArray.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int SingleElement(vector<int>& nums){
+    int n =nums.size();
     int st =0 ;
     int end =nums.size()-1;
+    if(n==1)return 0;
     while (st <= end) {
         int mid = st + (end - st) / 2;
-
+        if(mid==0&&nums[0]!=nums[1])return 0;
+        if(mid==(n-1)&&nums[n-1]!=nums[n-2])return n-1;
         // If mid is the single element
         if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
             return nums[mid];
